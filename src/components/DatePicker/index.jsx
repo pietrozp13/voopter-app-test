4f869c9dca18b928946ec5bde26a2bd0117f443e
Pietro Perboni <pietro.perboni@lighthouseit.com.br>
2023-02-27 15:58:40 -0300

    add eslint and prettier configs

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
-import {LocaleConfig} from 'react-native-calendars';
+import { LocaleConfig } from 'react-native-calendars';
 
 LocaleConfig.locales['pt-br'] = {
   monthNames: [
@@ -52,31 +52,31 @@ const COLORS = {
   DAY1: '#8936b3',
   DAY2: '#36b336',
   DAY3: '#ffb84d',
-  DAY4: '#26587d'
-}
+  DAY4: '#26587d',
+};
 
 export default function DatePicker() {
-    return (
-        <Calendar
-          // Collection of dates that have to be marked. Default = {}
-          initialDate={'2023-02-26'}
-          theme={{
-            arrowColor: '#00aad6',
-            'stylesheet.calendar.header': {
-              week: {
-                marginTop: 10,
-                marginBottom: 100,
-                flexDirection: 'row',
-                justifyContent: 'space-between'
-              }
-            }
-          }}
-          markedDates={{
-            '2023-02-26': {selected: true, marked: false, selectedColor: COLORS.DAY1},
-            '2023-02-27': {selected: true, marked: false, selectedColor: COLORS.DAY2},
-            '2023-02-28': {selected: true, marked: false, selectedColor: COLORS.DAY3},
-            '2023-03-01': {selected: true, marked: false, selectedColor: COLORS.DAY4},
-          }}
-        />
-    );
-  }
+  return (
+    <Calendar
+      // Collection of dates that have to be marked. Default = {}
+      initialDate={'2023-02-26'}
+      theme={{
+        arrowColor: '#00aad6',
+        'stylesheet.calendar.header': {
+          week: {
+            marginTop: 10,
+            marginBottom: 100,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          },
+        },
+      }}
+      markedDates={{
+        '2023-02-26': { selected: true, marked: false, selectedColor: COLORS.DAY1 },
+        '2023-02-27': { selected: true, marked: false, selectedColor: COLORS.DAY2 },
+        '2023-02-28': { selected: true, marked: false, selectedColor: COLORS.DAY3 },
+        '2023-03-01': { selected: true, marked: false, selectedColor: COLORS.DAY4 },
+      }}
+    />
+  );
+}
